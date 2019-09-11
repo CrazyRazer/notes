4b7c60c8bd80c0c76474224e9d9bfe95991c170b
Kostiantyn <crazyrazer@gmail.com>
2019-09-11 18:01:43 +0300

    Delete HelloWorld.ps1

--- HelloWorld.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-[string]$greeter,
-[string]$trigger
-)
-Write-Host "Hello world" from $greeter
-Write-Host Trigger: $trigger
-Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code."
-#something
